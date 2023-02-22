10bd5055c3528dc67b41b2dc2faacc536246a175
kiboma <omayioben@gmail.com>
2023-02-22 16:29:12 +0300

    use v-model to track inputs

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,18 +2,18 @@
   <form>
     <div>
         <label>Name</label>
-        <input type="name">
+        <input type="name" v-model="name">
     </div>
 
     <div>
         <label>Email</label>
-        <input type="email">
+        <input type="email" v-model="email">
     </div>
 
 
     <div>
         <label>Password</label>
-        <input type="password">
+        <input type="password" v-model="password">
     </div>
 
 
@@ -22,7 +22,13 @@
 
 <script>
 export default {
-    
+    data() {
+        return {
+            name: "Ben",
+            email: "admin@localhost.com",
+            password: "password1!"
+        }
+    }
 
 }
 </script>
